8caaf7c176669e9fc0efa4a4c0328ffd16865b82
Omerta85 <666Omerta@gmail.com>
2022-10-15 19:49:46 +0300

    finish

--- src/stack.js
+++ src/stack.js
@@ -12,20 +12,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * stack.pop(); // undefined
  *
  */
-class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ class Stack {
+  _arr = []
+
+  push(element) {
+    this._arr = this._arr.concat(element)
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    let res = this._arr[this._arr.length - 1]
+    this._arr = this._arr.slice(0, this._arr.length - 1)
+    return res
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(this._arr.length == 0) return undefined
+    return this._arr[this._arr.length - 1]
   }
 }
 
